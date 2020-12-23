82dd2ef04296396f280a2f41707a4eb5010b6e78
Justin Mason <justin@stacysMacAirs-MacBook-Air.local>
2020-12-23 15:10:23 -0700

    Added Counter for COmmit Window

--- KafkaFacade.Tests/CommitWindowTests.cs
+++ KafkaFacade.Tests/CommitWindowTests.cs
@@ -69,5 +69,78 @@ namespace KafkaFacade.Tests
             Assert.False(commitWindow.Elasped); 
 
         }
+
+        [Fact]
+        public void When_Initialized_Elapsed_Event_Fires(){
+            var clockMock = new Mock<IClock>();
+            clockMock.Setup(x=> x.Now ).Returns(new System.DateTime(1999,12,31,1,1,1,1));
+            var count = 0;
+      
+            var commitWindow = new CommitWindow(clockMock.Object);
+            commitWindow.WindowElapsed += (e,a) =>{
+                count++;
+            };
+            commitWindow.WindowMilliseconds = 200;
+            Assert.Equal (count, 0);
+            System.Threading.Thread.Sleep(200);
+            Assert.Equal (count, 1);             
+        }
+
+        [Fact]
+        public void When_Initialized_And_EventCount_Threshold_Met_Elasped(){
+
+            var clockMock = new Mock<IClock>();
+
+            clockMock.Setup(x=> x.Now ).Returns(new System.DateTime(1999,12,31,1,1,1,1));
+            Assert.Equal(new System.DateTime(1999,12,31,1,1,1,1), clockMock.Object.Now);
+
+            var commitWindow = new CommitWindow(clockMock.Object);
+            commitWindow.WindowMilliseconds = 10;
+            commitWindow.EventCountThreshold = 100;
+            commitWindow.Recieved();
+
+            for (int i = 0; i < 98; i++)
+            {            
+                commitWindow.Recieved();
+            }
+            Assert.False(commitWindow.Elasped);       
+
+            commitWindow.Recieved();
+            
+            Assert.True(commitWindow.Elasped);       
+        }
+
+        
+
+        public void When_EventCount_Threshol_Met_Elasped_Fired()
+        {
+            var clockMock = new Mock<IClock>();
+            var count = 0;
+
+            clockMock.Setup(x=> x.Now ).Returns(new System.DateTime(1999,12,31,1,1,1,1));
+            Assert.Equal(new System.DateTime(1999,12,31,1,1,1,1), clockMock.Object.Now);
+
+            var commitWindow = new CommitWindow(clockMock.Object);
+
+            commitWindow.WindowElapsed += (e,a) =>{
+                count++;
+            };
+
+            //Defaults raised elasped when initialized
+            Assert.Equal(1, count);
+
+            for (int i = 0; i < 98; i++)
+            {            
+                commitWindow.Recieved();
+            }
+
+            //Not raised untill 100 -  Default
+            Assert.Equal(1, count);
+            commitWindow.Recieved();
+            
+            Assert.Equal(2, count);
+            
+
+        }
     }
 }
